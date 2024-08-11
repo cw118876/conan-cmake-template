8398c3b2766a643f029ecbe8ce04331a566b8fa7
Dany <cw118876@gmail.com>
2024-08-11 22:00:58 +0800

    feat: add code check including cpplint, clang-tidy/format
    fix: remove unused link parameter that doesn't work for clang-tidy

--- src/{{project}}.cpp
+++ src/{{project}}.cpp
@@ -1,7 +1,9 @@
 #include "{{project}}/{{project}}.hpp"
 #include <iostream>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
+  (void)argc;
+  (void)argv;
   std::cout << "hello world\n";
   return 0;
 }
